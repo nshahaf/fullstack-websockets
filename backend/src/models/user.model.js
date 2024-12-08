e34f3dfe898e5c9d215e009b785ef294c07f2ddc
shahaf noked <shahafnoked@gmail.com>
2024-12-08 15:02:59 +0200

    mongoDB integration

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -2,21 +2,12 @@ import mongoose from "mongoose"
 
 const userSchema = new mongoose.Schema(
     {
-        email: { type: String, required: true, unique: true, lowercase: true, match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})$/, 'Please fill a valid email address'] },
-        fullName: { type: String, required: true, set: capitalizeFullName },
-        password: { type: String, required: true, minlength: 6 },
-        profilePic: { type: String, default: "" },
+        Name: { type: String, required: true },
+        Role: { type: String, default: "student", enum: ["student", "mentor"], required: true },
     },
     { timestamps: true }
 )
 
-//setters
-function capitalizeFullName(fullName) {
-    return fullName
-        .split(' ')
-        .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
-        .join(' ');
-}
 
 const User = mongoose.model("User", userSchema)
 export default User
--- backend/src/server.js
+++ backend/src/server.js
@@ -44,5 +44,5 @@ const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-  // connectDatabase()
+  connectDatabase()
 });
